a1a0a2261930b9ddb42380d9656877bec8438b17
michaelrizzo2 <m_rizzo@live.com>
2017-12-05 12:54:11 -0500

    made post to website and printed status code of 200

--- http_post.py
+++ http_post.py
@@ -2,4 +2,7 @@
 import requests as req
 #we need to get the values that we need to post to the website
 values={"firstname":"abc","lastname":"xyz"}
-
+#Now we need to post the data to the website
+r=req.post("https://www.example.com",data=values)
+print(r)
+#Response successful http code of 200
