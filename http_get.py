56784a192194b11522f93f0e4238b1e4ec54299b
michaelrizzo2 <m_rizzo@live.com>
2017-12-05 12:29:46 -0500

    made get request for google.com

---
+++ http_get.py
@@ -0,0 +1,11 @@
+#First we need to import the request module
+
+import requests
+
+#Now we need to make a get request
+
+r=requests.get("http://www.google.com")
+
+#NOw we need to get the statuts of the webpage
+print (r.status_code)
+#A status code of 200 means the request has been successful
