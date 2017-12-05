6a275483a0d1f630eda1dfc8a2eadec0ae7c085b
michaelrizzo2 <m_rizzo@live.com>
2017-12-05 12:38:50 -0500

    added code to get type of content for website

--- http_get.py
+++ http_get.py
@@ -9,3 +9,5 @@ r=requests.get("http://www.google.com")
 #NOw we need to get the statuts of the webpage
 print (r.status_code)
 #A status code of 200 means the request has been successful
+print (r.headers['content-type'])
+#We get that the content is html code
