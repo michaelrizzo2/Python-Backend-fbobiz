0b91fa6b47fa607d49b986d9c04c07e6786676a4
michaelrizzo2 <m_rizzo@live.com>
2017-12-05 12:50:05 -0500

    Made values to post to website

--- http_post.py
+++ http_post.py
@@ -1,3 +1,5 @@
 #we need to import the requests module to make a post request
 import requests as req
+#we need to get the values that we need to post to the website
+values={"firstname":"abc","lastname":"xyz"}
 
