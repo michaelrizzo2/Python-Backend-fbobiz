2ebb215a3c87826abbe2189fec2f56075788df12
michaelrizzo2 <m_rizzo@live.com>
2017-12-05 19:14:05 -0500

    added model and layers to the model

--- neural_network.py
+++ neural_network.py
@@ -8,4 +8,8 @@ data_set=numpy.loadtxt("pima-indians-diabetes.csv",delimiter=",")
 #split the dataset into x and y variables
 x=data_set[:,0:8]
 y=data_set[:,8]
-
+#Now we need to create the model
+model=Sequential()
+model.add(Dense(12,input_dim=8,init="uniform",activation="relu"))
+model.add(Dense(8,activation="relu"))
+model.add(Dense(1,activation="sigmoid"))
